575ae918bd431de350927ee0fa8bae2a18e662aa
Devishakti2230 <devishakti2000@gmail.com>
2021-04-16 13:45:43 +0530

    Update arithmetic_implementation.c

--- 3_Implementation/src/arithmetic_implementation.c
+++ 3_Implementation/src/arithmetic_implementation.c
@@ -4,6 +4,7 @@
 #include<stdlib.h>
 
 static result ans={0};
+static result_1 anss={0};
 void arthmetic_operation()
 {
     /**
@@ -11,6 +12,7 @@ void arthmetic_operation()
      * 
      */
      calculator_arithmetic value;
+     calculator_arithmetic_1 value_1;
     
     int choice=-1;
     int local_value1,local_value2;
@@ -25,11 +27,23 @@ void arthmetic_operation()
         if(choice==7)
         {
             exit(0);
+        }    
+	else if(choice==1 || choice==2 || choice==3 || choice==4 || choice==6)
+        {
+             printf("\nEnter the  two values \n");
+             scanf("%d %d",&local_value1,&local_value2);
+             value.number_1=local_value1;
+             value.number_2=local_value2;
         }
-        printf("\nEnter two values\n");
-        scanf("%d %d",&local_value1,&local_value2);
-        value.number_1=local_value1;
-        value.number_2=local_value2;
+        else if(choice==5)
+        {
+            
+             printf("\nEnter the  two values \n");
+             scanf("%d %d",&local_value1,&local_value2);
+             value_1.number_1=local_value1;
+             value_1.number_2=local_value2;
+        }
+	
         switch(choice)
         {
             /**
@@ -61,8 +75,8 @@ void arthmetic_operation()
             break;
 			
 	    case MOD:
-            modd(value,&ans);
-            printf("\nThe modulous is: %.2f",ans.final_answer);
+            modd(value_1,&anss);
+            printf("\nThe modulous is: %d",anss.final_answer);
             printf("\n");
             break;
 			
